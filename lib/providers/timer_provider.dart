a59d24334428aba1709ae28e98d15899b8652667
Hamdi Tuğ <hamditugmobil@gmail.com>
2025-09-02 12:24:52 +0300

    fix

--- lib/providers/timer_provider.dart
+++ lib/providers/timer_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import '../models/task_session.dart';
 import '../services/storage_service.dart';
 import '../services/webhook_service.dart';
